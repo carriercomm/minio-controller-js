b413548aab7bb4a5e696325462ff5093ded18b09
Harshavardhana <harsha@minio.io>
2015-09-22 15:15:11 -0700

    Add new examples, change some naming conventions.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import JsonRPC from './jsonrpc'
+/*
+ * Isomorphic Javascript library for Minio Controller API, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import JSONrpc from './jsonrpc'
 
 export default class Controller {
     constructor(endpoint) {
         var namespace = 'Controller'
-        this.JsonRPC = new JsonRPC({endpoint, namespace})
+        this.JSONrpc = new JSONrpc({endpoint, namespace})
     }
     makeCall(method, options) {
-        return this.JsonRPC.call(method, {params:[options]})
+        return this.JSONrpc.call(method, {params:[options]})
     }
     GenerateAuth(user) {
         return this.makeCall("GenerateAuth", {user});
